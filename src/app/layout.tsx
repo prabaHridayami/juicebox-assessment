a1a287eaeb801974041c0a0add5f58b230fd0099
Praba Hridayami <prabahridayami97@gmail.com>
2024-08-15 21:42:17 +0800

    added lottie file  and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { bagoss, sohne, graphik, agrandir } from "@/fonts";
 
-const inter = Inter({ subsets: ["latin"] });
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${bagoss.variable} ${sohne.variable} ${graphik.variable} ${agrandir.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/fonts/index.ts
@@ -0,0 +1,62 @@
+import localFont from "next/font/local";
+
+const bagoss = localFont({
+  src: [
+    {
+      path: "./Bagoss/Bagoss.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-bagoss",
+});
+
+const sohne = localFont({
+  src: [
+    {
+      path: "./Sohne (Body)/Sohne-Buch.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-sohne",
+});
+
+const graphik = localFont({
+  src: [
+    {
+      path: "./Graphik/Graphik-Medium.eot",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-graphik",
+});
+
+const agrandir = localFont({
+  src: [
+    {
+      path: "./PP Agrandir/PPAgrandir-Medium.eot",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./PP Agrandir/PPAgrandir-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./PP Agrandir/PPAgrandir-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./PP Agrandir/PPAgrandir-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-agrandir",
+});
+
+export { bagoss, sohne, graphik, agrandir };
